116dee6b08bed2d2fc3efc36d986b0ec2f794b09
awesome548 <kiku.uchikoga@gmail.com>
2023-01-21 11:42:18 +0900

    making dataset update

--- process/id_list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-wk_dir="/z/kiku/Basecaller/squiggle/RNN"
-tar_dir="/z/kiku/Dataset/Target"
-id_dir="/z/kiku/Dataset/ID"
-cd $wk_dir
-for i in A B C D E F;do
-    python preprocess/IDlist.py -t $tar_dir/$i -o $id_dir -f $i
-done
--- process/preprocess.py
+++ process/preprocess.py
@@ -22,26 +22,10 @@ def mad_normalization(data_test,filepath,maxlen):
     torch.save(data_test,filepath)
     return data_test
 
-def normalization(data_test,filepath,maxlen):
-    stats.zscore(data_test, axis=1, ddof=1)
-    x = np.where(np.abs(data_test) > 3.5)
-    for i in range(x[0].shape[0]):
-        if x[1][i] == 0:
-            data_test[x[0][i],x[1][i]] = data_test[x[0][i],x[1][i]+1]
-        elif x[1][i] == (maxlen-1):
-            data_test[x[0][i],x[1][i]] = data_test[x[0][i],x[1][i]-1]
-        else:
-            data_test[x[0][i],x[1][i]] = (data_test[x[0][i],x[1][i]-1] + data_test[x[0][i],x[1][i]+1])/2
-    data_test = torch.from_numpy(data_test.astype(np.float32)).clone()
-    torch.save(data_test,filepath)
-    print(torch.mean(data_test))
-    print(torch.std(data_test,unbiased=True))
-
-    return data_test
     
 
 def manipulate(x,cutlen,maxlen,size,stride):
-    num = calu_size(None,cutlen,maxlen,size,stride)
+    num = calu_size(cutlen,maxlen,stride)
     data = torch.zeros(size*num,cutlen)
     ### start point is already cutoff point ###
     for index in range(num):
@@ -59,7 +43,7 @@ def manipulate(x,cutlen,maxlen,size,stride):
     print(data.shape)
     return data
 
-def calu_size(cutoff,cutlen,maxlen,size,stride):
+def calu_size(cutlen,maxlen,stride):
     return (maxlen - cutlen)//stride + 1 
 
 
@@ -97,7 +81,7 @@ class Preprocess():
                         if len(raw_data) >= (cutoff + maxlen) and read.read_id in self.li:
                             arrpos.append(raw_data[cutoff:(cutoff + maxlen)])
                             count += 1
-            x = normalization(np.array(arrpos),filepath,maxlen) 
+            x = mad_normalization(np.array(arrpos),filepath,maxlen) 
         
         return manipulate(x,cutlen,maxlen,size,stride)
     
